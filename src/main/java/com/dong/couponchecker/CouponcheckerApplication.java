9a22168431e34539bf0c127ce7cbbf719ad71bc5
dong young <dong5854@gmail.com>
2023-08-11 21:58:08 +0900

    first commit

---
+++ src/main/java/com/dong/couponchecker/CouponcheckerApplication.java
@@ -0,0 +1,13 @@
+package com.dong.couponchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponcheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponcheckerApplication.class, args);
+	}
+
+}

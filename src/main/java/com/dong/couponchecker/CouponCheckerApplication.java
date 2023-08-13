b0b88fc112657a50be8398b67b179bdfde10a600
dong young <dong5854@gmail.com>
2023-08-13 22:44:52 +0900

    chore: configure database using docker compose

--- src/main/java/com/dong/couponchecker/CouponCheckerApplication.java
+++ src/main/java/com/dong/couponchecker/CouponCheckerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CouponcheckerApplication {
+public class CouponCheckerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CouponcheckerApplication.class, args);
+		SpringApplication.run(CouponCheckerApplication.class, args);
 	}
 
 }

27efb988f13b1b91695c1edf8647bebff02fb17f
dong young <dong5854@gmail.com>
2023-08-27 21:55:21 +0900

    feat: add user repository

---
+++ src/main/java/com/dong/couponchecker/domain/user/User.java
@@ -0,0 +1,32 @@
+package com.dong.couponchecker.domain.user;
+
+import com.dong.couponchecker.domain.coupon.Coupon;
+import com.dong.couponchecker.domain.club.Club;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.*;
+
+import java.util.*;
+import java.time.*;
+
+@NoArgsConstructor @AllArgsConstructor
+@Entity
+public class User {
+    @Id @GeneratedValue
+    private long id;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany
+    private List<Coupon> uploadedCoupons = new ArrayList<>();
+    @OneToMany
+    private List<Coupon> usedCoupons = new ArrayList<>();
+    @ManyToMany
+    private List<Club> clubs = new ArrayList<>();
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}

137bd7ad53aa12647edba79618d920107cc2548a
dong young <dong5854@gmail.com>
2023-08-30 01:54:09 +0900

    feat: add convenient method to Coupon entity

--- src/main/java/com/dong/couponchecker/domain/coupon/Coupon.java
+++ src/main/java/com/dong/couponchecker/domain/coupon/Coupon.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.*;
 
+@Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
 @Entity
 public class Coupon {
@@ -27,4 +28,28 @@ public class Coupon {
     // 일반적인 '유저'(user)가 아닌 '쿠폰을 사용한 사람'(user)라는 의미
     @ManyToOne
     private User user;
+
+    public void setUploader(User user) {
+        if(this.user != null) {
+            this.user.getUploadedCoupons().remove(this);
+        }
+        this.user = user;
+        user.getUploadedCoupons().add(this);
+    }
+
+    public void setClub(Club club) {
+        if(this.club != null) {
+            this.club.getCoupons().remove(this);
+        }
+        this.club = club;
+        club.getCoupons().add(this);
+    }
+
+    public void setUser(User user) {
+        if(this.user != null) {
+            this.user.getUsedCoupons().remove(this);
+        }
+        this.user = user;
+        user.getUsedCoupons().add(this);
+    }
 }

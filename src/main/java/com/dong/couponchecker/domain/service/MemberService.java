3ddeafb0cf8857541bda006768a52d7f51ba5d02
dong young <dong5854@gmail.com>
2023-09-28 23:30:53 +0900

    refactor: create 도중 문제 발생시 UnexpectedException 를 throw 하도록 수정

--- src/main/java/com/dong/couponchecker/domain/service/MemberService.java
+++ src/main/java/com/dong/couponchecker/domain/service/MemberService.java
@@ -2,9 +2,9 @@ package com.dong.couponchecker.domain.service;
 
 import com.dong.couponchecker.domain.Member;
 import com.dong.couponchecker.domain.MemberRepository;
+import com.dong.couponchecker.exception.UnexpectedException;
 import com.dong.couponchecker.util.SHA256;
 import com.dong.couponchecker.web.dto.MemberDto;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,18 +14,12 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    public boolean postMember(MemberDto memberDto) {
-        try {
+    public void postMember(MemberDto memberDto) throws UnexpectedException {
             Member member = Member.builder()
                     .name(memberDto.getName())
                     .email(memberDto.getEmail())
                     .password(SHA256.encrypt(memberDto.getPassword()))
                     .build();
             memberRepository.save(member);
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
         }
-    }
 }

ad9c4a46bb5ece60031300cab2a0b0eb9c1f10a4
dong young <dong5854@gmail.com>
2023-09-28 23:17:46 +0900

    refactor: NoSuchAlgorithmException 발생 시 UnexpectedException을 throw 하도록 변경
    정상적인 상황에서는 발생하면 안되는 오류이므로 UnexpectedException으로 감쌈

--- src/main/java/com/dong/couponchecker/util/SHA256.java
+++ src/main/java/com/dong/couponchecker/util/SHA256.java
@@ -1,15 +1,21 @@
 package com.dong.couponchecker.util;
 
+import com.dong.couponchecker.exception.UnexpectedException;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class SHA256 {
     // TODO: salt 추가 및 개선 필요
-    static public String encrypt(String text) throws NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
-        md.update(text.getBytes());
+    static public String encrypt(String text) throws UnexpectedException {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(text.getBytes());
 
-        return bytesToHex(md.digest());
+            return bytesToHex(md.digest());
+        } catch (NoSuchAlgorithmException e) {
+            throw new UnexpectedException();
+        }
     }
 
     static private String bytesToHex(byte[] bytes) {

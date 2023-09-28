60e0173408d16e7570aa54ad20df62229d6b6e83
dong young <dong5854@gmail.com>
2023-09-28 23:38:45 +0900

    feat: couponRespository에 findAllByUploader, findAllByClub 추가

--- src/main/java/com/dong/couponchecker/domain/CouponRepository.java
+++ src/main/java/com/dong/couponchecker/domain/CouponRepository.java
@@ -1,6 +1,9 @@
 package com.dong.couponchecker.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+    Optional<List<Coupon>> findAllByUploader(Member uploader);
+    Optional<List<Coupon>> findAllByClub(Club club);
 }

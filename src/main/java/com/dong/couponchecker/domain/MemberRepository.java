c84fe3659b663806df9495e29761add3918684e4
dong young <dong5854@gmail.com>
2023-10-02 17:40:09 +0900

    refactor: MemberRepository findByEmail 반환값 Optinal로 래핑

--- src/main/java/com/dong/couponchecker/domain/MemberRepository.java
+++ src/main/java/com/dong/couponchecker/domain/MemberRepository.java
@@ -2,7 +2,9 @@ package com.dong.couponchecker.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.*;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByEmail(String email);
+    Optional<Member> findByEmail(String email);
     void deleteMemberByEmail(String email);
 }

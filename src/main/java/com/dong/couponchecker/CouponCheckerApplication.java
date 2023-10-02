5554c629da506e1f30da4fd47d1f91346e27e555
dong young <dong5854@gmail.com>
2023-10-02 17:43:09 +0900

    config: JpaAuditingConfig 추가

--- src/main/java/com/dong/couponchecker/CouponCheckerApplication.java
+++ src/main/java/com/dong/couponchecker/CouponCheckerApplication.java
@@ -1,8 +1,11 @@
 package com.dong.couponchecker;
 
+import com.dong.couponchecker.config.JpaAuditingConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(JpaAuditingConfig.class)
 @SpringBootApplication
 public class CouponCheckerApplication {
 

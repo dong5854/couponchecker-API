c39316f9d263da231d43c1447c19a6ab13a55a8c
dong young <dong5854@gmail.com>
2023-10-02 17:33:56 +0900

    feat: jpa auditing config 추가

---
+++ src/main/java/com/dong/couponchecker/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.dong.couponchecker.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
